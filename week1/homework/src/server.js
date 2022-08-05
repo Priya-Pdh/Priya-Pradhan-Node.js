53bb7766004e842f91526c28c5c29a08ff37b36b
Priya Pradhan <pp9319660@gmail.com>
2022-08-05 12:33:22 +0200

    resolving the error to get expected result

--- week1/homework/src/server.js
+++ week1/homework/src/server.js
@@ -10,6 +10,30 @@ function createServer(port) {
 
   const server = http.createServer((request, response) => {
     // TODO: Write your homework code here
+    console.log(request.method, request.url);
+    switch (request.url) {
+      case '/state':
+        break;
+      case '/add':
+        state += 1;
+        break;
+      case '/subtract':
+        state -= 1;
+        break;
+      case '/reset':
+        state = 10;
+        break;
+      default:
+        const error = 'Not found';
+        response.statusCode = 404;
+        response.setHeader('Content-Type', 'application/json');
+        response.end(JSON.stringify({ error }));
+        return;
+    }
+    response.statusCode = 200;
+    response.setHeader('Content-Type', 'application/json');
+    response.end(JSON.stringify({ state }));
+
   });
 
   return server;

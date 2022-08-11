f2e1006ada66710cc9c5e3cda05726171fd6c72b
Priya Pradhan <pp9319660@gmail.com>
2022-08-11 10:59:47 +0200

    adding homework week2

--- week2/homework/src/index.js
+++ week2/homework/src/index.js
@@ -2,73 +2,41 @@
 
 // TODO: Write the homework code in this file
 
-'use strict'
+
+'use strict';
+
+// TODO: Write the homework code in this file
 
 let fs = require('fs');
 
 let args = process.argv.slice(2);
 let arg = args[0];
 
-if (!arg || arg == 'help') {
-  console.log(`Usage: node index.js [options]
-  HackYourFuture Node.js Week 2 - Lecture To-Do App
-  Options:
-    list          read all to-dos
-    add           add to-do
-    update        updates item on specific location
-    remove        removes specific entry at specific location
-    reset         resets file with removing all previous data
-    help          show this help text
-    `);
+if (!fs.existsSync('file.txt')) {
+  fs.writeFileSync('file.txt', JSON.stringify([]));
 }
 
-let add = fs.appendFile('file.txt', `${args.join(' ')}\n`, function (err) {
-  if (err) throw err;
-  console.log('Added new item!');
-});
+const data = fs.readFileSync('file.txt', 'utf-8');
+let todoList = data ? JSON.parse(data) : [];
 
-let toDoList = data ? JSON.parse(data) : [];
-let reset = fs.writeFile('file.txt', `${args.join(' ')}\n`, function (err, data) {
-  if (err) throw err;
-  console.log('Done writing!');
-});
-
-
-let list = fs.readFile('file.txt', function (err, data) {
-  if (err) throw err;
-  console.log("done reading");
-})
-
-let remove = fs.unlinkSync('file.txt', function (err, data) {
-  
-})
-
-let update = fs.readFileSync('file.txt', `${args.split('\n')}`, function (err) {
-  if (err) throw err;
-  console.log('Added new item!');
-})
-
-
-switch (args) {
+switch (arg) {
   case 'list': {
-    console.log(JSON.stringify(toDoList))
+    console.log(JSON.stringify(todoList))
     break;
   }
   case 'add': {
     todoList.push(args[1])
-    console.log(JSON.stringify(toDoList))
+    console.log(JSON.stringify(todoList))
     break;
   }
   case 'remove': {
-    todoList.splice(arg[1], 1)
-    fs.writeFileSync('list.txt', JSON.stringify(toDoList));
-    console.log(JSON.stringify(toDoList))
+    todoList.splice(args[1], 1)
+    console.log(JSON.stringify(todoList))
     break;
   }
   case 'reset': {
     todoList = [];
-    fs.writeFileSync(list.txt, JSON.stringify(toDoList));
-    console.log(JSON.stringify(toDoList))
+    console.log(JSON.stringify(todoList))
     break;
   }
   
@@ -77,6 +45,9 @@ switch (args) {
     break;
     }
 }
-
+fs.writeFile('file.txt', JSON.stringify(todoList), function (err) {
+  if (err) throw err;
+  console.log('Done writing!');
+});
 
 
